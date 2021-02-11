f5e6cb474d391feeea2524760835b53f166672ad
Steven Proctor <steven.proctor@gmail.com>
2021-02-10 20:43:48 -0600

    make Contact a Map instead of a Tuple

--- apps/contacts_server/lib/contact_server/contact.ex
+++ apps/contacts_server/lib/contact_server/contact.ex
@@ -6,13 +6,13 @@ defmodule ContactServer.Contact do
 	## Examples
 
     iex> ContactServer.Contact.from_line "Grayson | Dick | foo@example.com | blue | 1995-12-31\r\n"
-    {:ok, {:contact, {"Grayson", "Dick", "foo@example.com", "blue", ~D[1995-12-31]}}}
+    {:ok, %{last_name: "Grayson", first_name: "Dick", email: "foo@example.com", favorite_color: "blue", dob: ~D[1995-12-31]}}
 
     iex> ContactServer.Contact.from_line "Gordon, Barbara, bar@example.com, black, 1996-10-13"
-    {:ok, {:contact, {"Gordon", "Barbara", "bar@example.com", "black", ~D[1996-10-13]}}}
+    {:ok, %{last_name: "Gordon", first_name: "Barbara", email: "bar@example.com", favorite_color: "black", dob: ~D[1996-10-13]}}
 
     iex> ContactServer.Contact.from_line "Drake Tim baz@example.com red 2005-07-24"
-    {:ok, {:contact, {"Drake", "Tim", "baz@example.com", "red", ~D[2005-07-24]}}}
+    {:ok, %{last_name: "Drake", first_name: "Tim", email: "baz@example.com", favorite_color: "red", dob: ~D[2005-07-24]}}
 
     iex> ContactServer.Contact.from_line "Brown, Stephanie, baz@example.com, red, 2005/07/24"
     {:error, :invalid_birthdate_format}
@@ -44,7 +44,7 @@ defmodule ContactServer.Contact do
   defp make_contact({:ok, [last, first, email, color, dob_str]}) do
     case Date.from_iso8601(dob_str) do
       {:ok, dob } ->
-         {:ok, {:contact, {last, first, email, color, dob }}}
+         {:ok, %{last_name: last, first_name: first, email: email, favorite_color: color, dob: dob }}
       {:error, :invalid_format} ->
          {:error, :invalid_birthdate_format }
 		end
--- apps/contacts_server/test/contact_store_test.exs
+++ apps/contacts_server/test/contact_store_test.exs
@@ -7,14 +7,14 @@ defmodule ContactServer.ContactStoreTest do
   end
 
   test "adding a contact puts the contact in the store", %{store: store} do
-    contact = {"a", "b", "c", "d", ~D[2020-01-01]}
+    contact = %{last_name: "a", first_name: "b", email: "c", favorite_color: "d", dob: ~D[2020-01-01]}
     ContactServer.ContactStore.create(store, contact)
     {:ok, contacts} = ContactServer.ContactStore.get_all(store)
     assert list_contains(contacts, contact)
   end
 
   test "adding the same contact multiple times only puts the contact in the store once", %{store: store} do
-    contact = {"a", "b", "c", "d", ~D[2020-01-01]}
+    contact = %{last_name: "a", first_name: "b", email: "c", favorite_color: "d", dob: ~D[2020-01-01]}
     ContactServer.ContactStore.create(store, contact)
     ContactServer.ContactStore.create(store, contact)
     {:ok, contacts} = ContactServer.ContactStore.get_all(store)
@@ -22,8 +22,8 @@ defmodule ContactServer.ContactStoreTest do
   end
 
   test "adding the two contacts differing by date adds both contacts", %{store: store} do
-    contact1 = {"a", "b", "c", "d", ~D[2020-01-01]}
-    contact2 = {"a", "b", "c", "d", ~D[1920-01-01]}
+    contact1 = %{last_name: "a", first_name: "b", email: "c", favorite_color: "d", dob: ~D[2020-01-01]}
+    contact2 = %{last_name: "aa", first_name: "b", email: "c", favorite_color: "d", dob: ~D[1920-01-01]}
     ContactServer.ContactStore.create(store, contact1)
     ContactServer.ContactStore.create(store, contact2)
     {:ok, contacts} = ContactServer.ContactStore.get_all(store)
