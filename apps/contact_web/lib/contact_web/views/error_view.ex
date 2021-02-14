ecdf87a982a215774c791ec625937558268431ef
Steven Proctor <steven.proctor@gmail.com>
2021-02-13 20:14:13 -0600

    fix erroring test on 404 error

--- apps/contact_web/lib/contact_web/views/error_view.ex
+++ apps/contact_web/lib/contact_web/views/error_view.ex
@@ -8,7 +8,7 @@ defmodule ContactWeb.ErrorView do
   # end
 
   def render("404.html", _assigns) do
-    "Page Not Found"
+    "Not Found"
   end
 
   # By default, Phoenix returns the status message from
